3933875511fde03eb73e43031efcecdac15bd345
Nirvan004 <Nirvanandb2004@gmail.com>
2025-11-24 15:08:06 -0500

    added most variables i think i will need

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("ip-form");
+const input = document.getElementById("ip-input");
+const ipDisplay = document.getElementById("ip-display");
+const locationDisplay = document.getElementById("location-display");
+const timezoneDisplay = document.getElementById("timezone-display");
+const ispDisplay = document.getElementById("isp-display");
